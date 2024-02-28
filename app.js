021630be74c6f308b1659c613f2a0d08b2e8cc31
sophie1629 <sophielauwe@gmail.com>
2024-02-27 21:54:05 -0300

    scripts en json

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const port =process.env.PORT || 3030;
+
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
